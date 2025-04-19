2de39000348ac67996493686bdfafd0c2d98b43d
ILRSBandara <rashminisaundarya19@gmail.com>
2025-04-19 13:12:10 +0530

    Created the login screen

--- lib/Screen/loding.dart
+++ lib/Screen/loding.dart
@@ -1,65 +1,123 @@
 import 'dart:async'; // Import Timer
+import 'package:aura_care/Screen/login.dart';
 import 'package:flutter/material.dart';
-import 'dashboard.dart'; // Import your dashboard file
 
-class LodingScreen extends StatefulWidget {
-  const LodingScreen({super.key});
+class LodingScreen
+    extends
+        StatefulWidget {
+  const LodingScreen({
+    super.key,
+  });
 
   @override
   // ignore: library_private_types_in_public_api
-  _LodingScreenState createState() => _LodingScreenState();
+  _LodingScreenState
+  createState() =>
+      _LodingScreenState();
 }
 
-class _LodingScreenState extends State<LodingScreen> {
+class _LodingScreenState
+    extends
+        State<
+          LodingScreen
+        > {
   @override
-  void initState() {
-    super.initState();
+  void
+  initState() {
+    super
+        .initState();
     // Delay for 3 seconds, then navigate to DashboardScreen
-    Timer(const Duration(seconds: 3), () {
-      // Replace the current screen with DashboardScreen
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const DashboardScreen()),
-      );
-    });
+    Timer(
+      const Duration(
+        seconds:
+            3,
+      ),
+      () {
+        // Replace the current screen with DashboardScreen
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder:
+                (
+                  context,
+                ) =>
+                    const LogIn(),
+          ),
+        );
+      },
+    );
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget
+  build(
+    BuildContext
+    context,
+  ) {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
-              const Color.fromARGB(223, 138, 221, 185),
-              const Color.fromARGB(224, 45, 139, 108),
+              const Color.fromARGB(
+                223,
+                138,
+                221,
+                185,
+              ),
+              const Color.fromARGB(
+                224,
+                45,
+                139,
+                108,
+              ),
             ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+            begin:
+                Alignment.topCenter,
+            end:
+                Alignment.bottomCenter,
           ),
         ),
         child: Center(
           child: Padding(
-            padding: const EdgeInsets.only(top: 300),
+            padding: const EdgeInsets.only(
+              top:
+                  300,
+            ),
             child: Column(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(bottom: 105),
+                  padding: const EdgeInsets.only(
+                    bottom:
+                        105,
+                  ),
                   child: Image.asset(
                     'assets/Images/abcd.png',
-                    width: 770,
-                    height: 300,
+                    width:
+                        770,
+                    height:
+                        300,
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 90),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal:
+                        90,
+                  ),
                   child: const CircularProgressIndicator(
-                    color: Colors.white,
-                    strokeWidth: 5,
+                    color:
+                        Colors.white,
+                    strokeWidth:
+                        5,
                   ),
                 ),
-                const SizedBox(height: 10),
-                const Text('Loading....'),
+                const SizedBox(
+                  height:
+                      10,
+                ),
+                const Text(
+                  'Loading....',
+                ),
               ],
             ),
           ),
