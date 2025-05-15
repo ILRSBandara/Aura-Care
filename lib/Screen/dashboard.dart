abf6bb497002676a20e69c3eedcb98fd67e07462
ILRSBandara <160006775+ILRSBandara@users.noreply.github.com>
2025-05-16 01:15:43 +0530

    Updated chatbot

--- lib/Screen/dashboard.dart
+++ lib/Screen/dashboard.dart
@@ -1,144 +1,77 @@
-import 'package:aura_care/Screen/chatBot/geminiChatBot.dart';
 import 'package:aura_care/Screen/home.dart';
-import 'package:aura_care/Screen/menu.dart';
 import 'package:aura_care/Screen/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class DashboardScreen
-    extends
-        StatefulWidget {
-  final String
-  emaill;
-  const DashboardScreen({
-    super.key,
-    required this.emaill,
-  });
+import 'chatBot/chatPage.dart';
+
+class DashboardScreen extends StatefulWidget {
+  final String emaill;
+  const DashboardScreen({super.key, required this.emaill});
 
   @override
-  State<
-    DashboardScreen
-  >
-  createState() =>
-      _DashboardScreenState();
+  State<DashboardScreen> createState() => _DashboardScreenState();
 }
 
-class _DashboardScreenState
-    extends
-        State<
-          DashboardScreen
-        > {
-  int
-  _selectedIndex =
-      0;
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
 
-  late List<
-    Widget
-  >
-  _pages;
+  late List<Widget> _pages;
 
   @override
-  void
-  initState() {
-    super
-        .initState();
-    _pages = <
-      Widget
-    >[
+  void initState() {
+    super.initState();
+    _pages = <Widget>[
       HomePage(), // index 0
-      Center(
-        child: Text(
-          'Reminder',
-        ),
-      ),
-      //GeminiChatBot(),
+      Center(child: Text('Reminder')),
+      ChatPage(),
       ProfileDisplayData(),
     ];
   }
 
-  void
-  _onItemTapped(
-    int
-    index,
-  ) {
+  void _onItemTapped(int index) {
     setState(() {
-      _selectedIndex =
-          index;
+      _selectedIndex = index;
     });
   }
 
   @override
-  Widget
-  build(
-    BuildContext
-    context,
-  ) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      body:
-          _pages[_selectedIndex],
+      backgroundColor: Colors.white,
+      body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex:
-            _selectedIndex,
-        onTap:
-            _onItemTapped,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-            ),
-            label:
-                'Home',
-            backgroundColor:
-                Colors.white,
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.white,
           ),
           BottomNavigationBarItem(
-            icon: Icon(
-              Icons.punch_clock,
-            ),
-            label:
-                'Reminder',
-            backgroundColor:
-                Colors.white,
+            icon: Icon(Icons.punch_clock),
+            label: 'Reminder',
+            backgroundColor: Colors.white,
           ),
           BottomNavigationBarItem(
-            icon: FaIcon(
-              FontAwesomeIcons.robot,
-              size:
-                  23.0,
-            ),
-            label:
-                'Chatbot',
-            backgroundColor:
-                Colors.white,
+            icon: FaIcon(FontAwesomeIcons.robot, size: 23.0),
+            label: 'Chatbot',
+            backgroundColor: Colors.white,
           ),
           BottomNavigationBarItem(
-            icon: Icon(
-              Icons.account_circle_rounded,
-            ),
-            label:
-                'Profile',
-            backgroundColor:
-                Colors.white,
+            icon: Icon(Icons.account_circle_rounded),
+            label: 'Profile',
+            backgroundColor: Colors.white,
           ),
         ],
-        unselectedItemColor:
-            Colors.grey,
-        selectedItemColor:
-            Colors.green,
-        showSelectedLabels:
-            true,
-        showUnselectedLabels:
-            true,
-        selectedLabelStyle: TextStyle(
-          fontSize:
-              12,
-        ),
-        unselectedLabelStyle: TextStyle(
-          fontSize:
-              12,
-        ),
-        type:
-            BottomNavigationBarType.fixed,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.green,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedLabelStyle: TextStyle(fontSize: 12),
+        unselectedLabelStyle: TextStyle(fontSize: 12),
+        type: BottomNavigationBarType.fixed,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,12 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_dotenv/flutter_dotenv.dart';
-// import 'chat_screen.dart'; // Import your ChatScreen
-
-// void
-// main() async {
-//   WidgetsFlutterBinding.ensureInitialized(); // Add this line
-//   await dotenv.load(
-//     fileName:
-//         ".env",
-//   ); // Load .env file
-//   runApp(
-//     const MyApp(),
-//   );
-// }
-
-// class MyApp
-//     extends
-//         StatelessWidget {
-//   const MyApp({
-//     super.key,
-//   });
-
-//   @override
-//   Widget
-//   build(
-//     BuildContext
-//     context,
-//   ) {
-//     return MaterialApp(
-//       title:
-//           'Gemini Chatbot',
-//       theme: ThemeData(
-//         primarySwatch:
-//             Colors.blue,
-//         visualDensity:
-//             VisualDensity.adaptivePlatformDensity,
-//         fontFamily:
-//             'GoogleSans',
-//       ),
-//       home:
-//           const ChatScreen(), // Use the ChatScreen widget
-//     );
-//   }
-// }
-
-import 'package:aura_care/ui/chat.dart';
+import 'package:aura_care/Screen/dashboard.dart';
+import 'package:aura_care/Screen/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-import 'ui/home.dart';
-
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -59,16 +16,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chat Bot',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
-        useMaterial3: true,
-      ),
-      initialRoute: HomePage.routeName,
-      routes: {
-        HomePage.routeName: (context) => const HomePage(),
-        ChatPage.routeName: (context) => const ChatPage(),
-      },
+      debugShowCheckedModeBanner: false,
+      title: 'Aura Care',
+      home: const DashboardScreen(emaill: 'hello@gmail.com'),
     );
   }
 }
+
+
+// import 'package:aura_care/ui/chat.dart';
+// import 'package:flutter/material.dart';
+
+// import 'ui/home.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Chat Bot',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
+//         useMaterial3: true,
+//       ),
+//       initialRoute: HomePage.routeName,
+//       routes: {
+//         HomePage.routeName: (context) => const HomePage(),
+//         ChatPage.routeName: (context) => const ChatPage(),
+//       },
+//     );
+//   }
+// }
